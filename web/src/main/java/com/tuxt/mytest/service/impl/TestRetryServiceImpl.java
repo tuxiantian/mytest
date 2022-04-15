43b423ef43c32e88aa328c6e0f70bfbfe77cfa88
wb-txt666396 <wb-txt666396@alibaba-inc.com>
2022-04-15 16:53:58 +0800

    换成log打印日志

--- web/src/main/java/com/tuxt/mytest/service/impl/TestRetryServiceImpl.java
+++ web/src/main/java/com/tuxt/mytest/service/impl/TestRetryServiceImpl.java
@@ -1,30 +1,33 @@
 package com.tuxt.mytest.service.impl;
 
 import com.tuxt.mytest.service.TestRetryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalTime;
- 
+
 @Service
+@Slf4j
 public class TestRetryServiceImpl implements TestRetryService {
- 
+
     @Override
-    @Retryable(value = Exception.class,maxAttempts = 3,backoff = @Backoff(delay = 2000,multiplier = 1.5))
-    public int test(int code) throws Exception{
-        System.out.println("test被调用,时间："+LocalTime.now());
-          if (code==0){
-              throw new Exception("情况不对头！");
-          }
-        System.out.println("test被调用,情况对头了！");
- 
+    @Retryable(value = Exception.class, maxAttempts = 3, backoff = @Backoff(delay = 2000, multiplier = 1.5))
+    public int test(int code) throws Exception {
+        log.info("test被调用,时间：" + LocalTime.now());
+        if (code == 0) {
+            throw new Exception("情况不对头！");
+        }
+        log.info("test被调用,情况对头了！");
+
         return 200;
     }
 
     @Recover
-    public int recover(Exception e, int code){
-        System.out.println("回调方法执行！！！！");
+    public int recover(Exception e, int code) {
+        log.info("回调方法执行！！！！");
         //记日志到数据库 或者调用其余的方法
         return 400;
     }
